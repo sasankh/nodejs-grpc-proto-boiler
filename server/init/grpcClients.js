d41bbfa7920621dc0c89211d87022318bc488e97
bcsasankh@gmail.com <bcsasankh@gmail.com>
2018-12-18 21:59:35 -0800

    added the node client code

--- server/init/grpcClients.js
+++ server/init/grpcClients.js
@@ -1,70 +1,84 @@
-// 'use strict';
-//
-// const config = require(`${global.__base}/server/config/config`);
-//
-// const {
-//   initializeClient
-// } = require('./protoLoader');
-//
-// const external = config.proto.external;
-// const serviceOptions = {
-//   keepCase: true,
-//   longs: String,
-//   enums: String,
-//   defaults: true,
-//   oneofs: true,
-//   includeDirs: [
-//     config.proto.external
-//   ]
-// };
-//
-// // set ther service config here
-// const serviceConfig = {
-//   // external service 1
-//   rpcServiceName1: {
-//     protoFile:  external.sampleServiceExample1.protoFileName1,
-//     options: Object.assign(serviceOptions, {
-//       includeDirs: [external.sampleServiceExample1.]
-//     })
-//
-//     protoPath: `${global.__base}/protos/internal`,
-//     protoFileName1: 'rpcs.proto',
-//     rpcServiceName1: 'GrpcBoilerPlate',
-//     sampleServiceAddress: process.env.SAMPLE_SERVICE_ADDRESS || 'localhost:50051'  // this is just sample address
-//
-//
-//   }
-// };
-//
-// const services = {};
-// let servicesList = [];
-//
-// protoFile, options, service, serviceUrl
-//
-// async function initialize() {
-//   externalServices.rpcServiceName1 = await initializeClient(
-//     'INITIALIZE',
-//
-//   );
-// }
-//
-// initialize();
-//
-// function getClient(type) => {
-//   return new Promise((resolve, reject) => {
-//     try {
-//       if (externalServices.includes(type)) {
-//
-//       } else {
-//         reject(
-//           error: {
-//             level: 'warn',
-//             message: 'Cannot return client as requested service not listed in external service list'
-//           }
-//         )
-//       }
-//     } catch (e) {
-//       reject(e);
-//     }
-//   }
-// }
+'use strict';
+
+const config = require(`${global.__base}/server/config/config`);
+
+const {
+  initializeClient
+} = require('./protoLoader');
+
+const clients = {};
+
+const protoOptions = {
+  keepCase: true,
+  longs: String,
+  enums: String,
+  defaults: true,
+  oneofs: true,
+  includeDirs: config.proto.protoDirToInclude
+};
+
+const external = config.proto.external;
+
+// set the service config here
+const serviceConfig = {
+  rpcServiceName1: {
+    protoFile:  external.sampleServiceExample1.protoFileName1,
+    options: protoOptions,
+    service: external.sampleServiceExample1.rpcServiceName1,
+    serviceUrl: external.sampleServiceExample1.sampleServiceAddress
+  }
+};
+
+let clientList;
+
+function initializeIndividual(service) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      clients[service] = await initializeClient(
+        serviceConfig[service].protoFile,
+        serviceConfig[service].options,
+        serviceConfig[service].service,
+        serviceConfig[service].serviceUrl
+      );
+    } catch (e) {
+      reject(e);
+    }
+  });
+}
+function initialize() {
+  clientList = Object.keys(serviceConfig);
+
+  clientList.forEach(async (service) => {
+    await initializeIndividual(service);
+  });
+}
+
+function getClient(client) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      if (clientList.includes(client)) {
+        if (!clients[client]) {
+          await initializeIndividual(service);
+        }
+
+        resolve(clients[client]);
+      } else {
+        reject({
+          error: {
+            level: 'warn',
+            message: 'Cannot return client as requested service not listed in external service list'
+          }
+        });
+      }
+    } catch (e) {
+      reject(e);
+    }
+  });
+}
+
+initialize();
+
+module.exports = {
+  getClient,
+  initializeClient
+}
